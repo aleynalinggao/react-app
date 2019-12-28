2226c566d7f4930b2f815f9bc01d756d99501bf4
aleynaling <aleyan.gao@atalent.com>
2019-12-28 18:33:51 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
+      <div className="container">
+        <div className="row">
+            <div className="col-xs-4  col-offset-8">
+                <h2>I m app component</h2>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-xs-4  col-offset-8">
+                <Home />
+            </div>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+        <div className="container">
+          <div className="row">
+              <div className="col-xs-4  col-offset-8">
+                  <h2>Home's page</h2>
+              </div>
+          </div>
+        </div>
+    );
+  }
+}
